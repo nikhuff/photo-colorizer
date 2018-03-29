dedaa9fbd730e6524cba37a507eaf84d848a8b82
nikhuff <nik.huff@me.com>
2018-03-29 15:04:48 -0600

    started implementing keras

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 import numpy as np
-from sklearn.neural_network import MLPRegressor
-from sklearn.externals import joblib
+# from sklearn.neural_network import MLPRegressor
+# from sklearn.externals import joblib
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, Activation
+from keras.wrappers.scikit_learn import KerasRegressor
+
+def model():
+    model = Sequential()
+    model.add(Dense(32, input_shape=(471,)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, 3, input_shape=(471, 500, 3)))
+    model.compile(optimizer='rmsprop', loss='mse')
+    return model
 
 def main():
     data_images, target_images = np.load('./assets/data.npy'), np.load('./assets/target.npy')
-    colorizer = MLPRegressor()
-    print(data_images[0].shape)
-    print(target_images[0].shape)
-    data = []
-    targets = []
-    for i, image in enumerate(data_images):
-        for row in range(1, len(image) - 1):
-            for column in range(1, len(image[row]) - 1):
-                kernal = np.zeros((3, 3))
-                kernal[0][0] = image[row - 1][column - 1]
-                kernal[0][1] = image[row - 1][column]
-                kernal[0][2] = image[row - 1][column + 1]
-                kernal[1][0] = image[row][column - 1]
-                kernal[1][1] = image[row][column]
-                kernal[1][2] = image[row][column + 1]
-                kernal[2][0] = image[row + 1][column - 1]
-                kernal[2][1] = image[row + 1][column]
-                kernal[2][2] = image[row + 1][column + 1]
+    print(data_images[0].shape, target_images[0].shape)
+    regressor = KerasRegressor(build_fn=model, nb_epoch=100)
+    regressor.fit(data_images[0], target_images)
 
-                data.append(kernal)
-                targets.append(target_images[i][row][column])
-
-    colorizer.fit(np.array(data), np.array(targets))
-    joblib.dump(colorizer, 'colorizer.pkl')
+    regressor.model.save('./assets/colorizer.h5')
                 
 if __name__ == '__main__':
     main()
